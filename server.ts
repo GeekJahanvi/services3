4da576eed6fbc7309f12af96111c8d58b6e62699
Jahanvi <jahanvi@geekyants.com>
2023-04-13 12:45:53 +0530

    PORT CHANGES

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import loader from './helpers/loader';
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })).use(bodyParser.json());
